320006a2f67a68a2186cbde17103c045aff2bbcc
Aman Maseeh Luqman KA <llukku879@gmail.com>
2025-11-04 10:45:35 +0530

    Add files via upload

---
+++ add 2 numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int myfunction(int x,int y)
+{
+int c:
+c=x+y;
+}
+int main()
+{
+int a,b;
+printf("Enter two numbers:");
+scanf("%d%d",&a,&b);
+int result=my function(a,b);
+print("Result is =%d",result);
+return 0;
+}
